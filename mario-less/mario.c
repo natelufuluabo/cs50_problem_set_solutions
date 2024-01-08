641287123d58bd2026f4afdb411c940ea5a41ecd
aircongo <natelufuluabo@yahoo.ca>
2024-01-07 19:36:20 -0500

    edited condition in mario-less so that user can only enter value superior to 0

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -4,11 +4,15 @@ int main(void)
 {
     int userInput;
     
-    // Prompt the user to enter a number
-    printf("Height: ");
-    
-    // Read the input from the user
-    scanf("%d", &userInput);
+    do 
+    {
+        // Prompt the user to enter a number
+        printf("Height: ");
+        
+        // Read the input from the user
+        scanf("%d", &userInput);
+    }
+    while (userInput <= 0);
 
     for (int i = 1; i <= userInput; i++)
     {
