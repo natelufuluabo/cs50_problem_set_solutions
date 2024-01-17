32a058877ccd418326551dd2350e42829c4d321f
aircongo <natelufuluabo@yahoo.ca>
2024-01-17 18:44:12 -0500

    implemented function used to count words from user input

--- problemSet2/readability/readibility.c
+++ problemSet2/readability/readibility.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
+int countWord(char input[]);
+
 int main(void)
 {
-    
+    char input[50];
+    printf("Enter a sentence: ");
+    fgets(input, sizeof(input), stdin);
+
+    int wordCounter = countWord(input);
+
+    return 0;
+}
+
+int countWord(char input[])
+{
+    char delimiter[] = " ";
+    char *word;
+    int counter = 0;
+
+    word = strtok(input, delimiter);
+
+    int i = 0;
+    while (word != NULL) {
+        i++;
+        counter++;
+        word = strtok(NULL, delimiter);
+    }
+
+    return counter;
 }
